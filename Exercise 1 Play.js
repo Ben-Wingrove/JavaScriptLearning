28a03fc0e6d2b698145e3c1ada89e1d72023df2d
Ben-Wingrove <68328490+Ben-Wingrove@users.noreply.github.com>
2021-11-18 16:11:49 +0000

    Add files via upload
    The Exercise task set by Mosh Hamedani was; 
    "Write a function that takes two numbers and returns the maximum of the two."
    
    After I understood how to write this function, I coded different calculation functions. In the near future I plan to make a basic calculator with this logic.

---
+++ Exercise 1 Play.js
@@ -0,0 +1,76 @@
+// Exercise 01
+
+let number = max(14, 12);
+console.log(number);
+
+function max(a, b) {
+  return a > b ? a : b;
+}
+
+// Play 01
+// Calculator (Thanks Aoife! <3)
+//
+
+//Add
+function add(a, b) {
+  return a + b;
+}
+
+let addition = add(2, 4);
+console.log(addition);
+console.log(add(2, 4));
+
+//Sub
+function sub(a, b) {
+  return a - b;
+}
+let subtraction = sub(4, 2);
+console.log(subtraction);
+console.log(sub(2, 4));
+
+//Multi
+function multi(a, b) {
+  return a * b;
+}
+let multiply = multi(2, 5);
+console.log(times);
+console.log(multi(2, 4));
+
+//Divi
+function divi(a, b) {
+  return a / b;
+}
+let divide = divi(3, 9);
+console.log(divide);
+console.log(divi(2, 4));
+
+//Butt0
+function butt0(a) {
+  return a;
+}
+let button0 = butt0(0);
+console.log(button0);
+console.log(butt0));
+
+//Butt1
+function butt1(a) {
+  return a;
+}
+let button1 = butt1(1);
+console.log(button1);
+console.log(butt1);
+
+//Butt2
+function butt2(a) {
+  return a;
+}
+let button2 = butt2(2);
+console.log(button2);
+console.log(butt2);
+
+//Add button 1 and button 2 
+function add(a, b) {
+  return a + b;
+}
+
+add(button1, button2);
